2a636ed358f7bd781100de575f53afc325f95466
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-03-20 10:08:53 -0500

    Fixed Json

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ let pythondef = document.getElementById("pythondef")
 let pythonanswer = document.getElementById('pythonanswer')
 let nextbutton = document.getElementById('nextbutton')
 
+
 let database = firebase.database()
 
-pythonflashcards.map(function(question){
-  console.log(question.Definition)
+let counter = 0
+
+nextbutton.addEventListener('click',function(){
+
+let questionItem = `<p> ${pythonflashcards[counter].Term}</p>`
+let solutionItem = `<p>${pythonflashcards[counter].Definition}</p>`
+counter += 1
+pythondef.innerHTML = questionItem
+pythonanswer.innerHTML = solutionItem
 })
