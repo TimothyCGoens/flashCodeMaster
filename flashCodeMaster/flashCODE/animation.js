5f46651af587e5c592b61c57dfe57edb156a0b08
Tim Goens <46302093+TimothyCGoens@users.noreply.github.com>
2019-03-23 11:09:49 -0500

    Add files via upload
    added slight styling changes and fixed the html database

---
+++ flashCodeMaster/flashCODE/animation.js
@@ -0,0 +1,106 @@
+$('#flashF').hide().delay().show()
+$('#flashL').hide().delay().show()
+$('#flashA').hide().delay().show()
+$('#flashS').hide().delay().show()
+$('#flashH').hide().delay().show()
+$('#sixthCard').hide().delay().show(2250)
+$('#seventhCard').hide().delay().show(2500)
+$('#eightCard').hide().delay().show(2750)
+$('#ninthCard').hide().delay().show(3000)
+
+
+$('#sixthCard').hide(5000).delay().show()
+$('#seventhCard').hide(6000).delay().show()
+$('#eightCard').hide(7000).delay().show()
+$('#ninthCard').hide(8000).delay().show()
+
+// $('#sixthCard').hide(5000).delay().show()
+// $('#seventhCard').hide(6000).delay().show()
+// $('#eightCard').hide(7000).delay().show()
+// $('#ninthCard').hide(8000).delay().show()
+//
+
+$('.cCard-flip-card').first().delay(4000).css({
+  'transition' : 'transform 5.0s',
+  'transform-style' : 'preserve-3d',
+  'transform': 'rotateY(180deg)'
+})
+$('.oCard-flip-card').delay(3000).css({
+  'transition' : 'transform 5.0s',
+  'transform-style' : 'preserve-3d',
+  'transform': 'rotateY(180deg)'
+})
+$('.dCard-flip-card').delay(3000).css({
+  'transition' : 'transform 5.0s',
+  'transform-style' : 'preserve-3d',
+  'transform': 'rotateY(180deg)'
+})
+$('.eCard-flip-card').delay(3000).css({
+  'transition' : 'transform 5.0s',
+  'transform-style' : 'preserve-3d',
+  'transform': 'rotateY(180deg)'
+})
+
+$('#firebaseui-auth-container').hide().delay(5000).fadeIn()
+
+
+
+
+
+// delay(3000).css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// })
+//
+// $(window).setTimeOut(function(){
+// $('#ninthCard').$(".eCard-flip-card").css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// })
+// },3000)
+
+
+//
+// $(".eCard-flip-card").css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// })
+// // }, 4000)
+//
+// // window.setTimeOut(function(){
+// $(".dCard-flip-card").css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// })
+// // // }, 4000)
+// //
+// $(".oCard-flip-card").css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// }, 4000)
+// //
+// $(".cCard-flip-card").css({
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// }, 4000)
+// //
+//
+// // },4000)
+
+//
+
+// {
+//   'transition' : 'transform 5.0s',
+//   'transform-style' : 'preserve-3d',
+//   'transform': 'rotateY(180deg)'
+// }
+
+// $(function() {
+//   $('#flipAnimation').animate(), 2000)
+// })
